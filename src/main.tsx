8e3bb3203dafa002e819c12de1eb2d468d9b6a58
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2025-05-04 23:07:30 +0530

    feat: add posthog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { PostHogProvider} from 'posthog-js/react'
+
+const options = {
+  api_host: "https://webhook.site/5adb4893-1356-4d0f-9208-c202b337b945",
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/**@ts-ignore */}
+     <PostHogProvider 
+      apiKey={"phc_wXgrqQTjRA8gdQqoWBDGHj598ky4o1RGIPWlyiVCYwJ"}
+      options={options}
+    >
     <App />
+    </PostHogProvider>
   </StrictMode>,
 )
