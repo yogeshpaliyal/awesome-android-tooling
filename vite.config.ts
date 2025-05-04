1b466329ede1e34bb5332767f8648cff0ab62cc0
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2025-05-04 20:42:09 +0530

    feat: setup github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/awesome-android-tooling/', // Set base path for GitHub Pages
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
